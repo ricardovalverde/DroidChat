1e7471d6136dc6b215f23e2924034f1ba2a685c7
ricardovalverde <valverde.ricardojr@gmail.com>
2026-04-26 22:46:07 -0300

    Implement navigation to Sign Up screen from Sign In
    - Update `ChatNavHost` to pass navigation logic from `SignInNav` to `SignUpNav`.
    - Add `navigateToSignUp` and `onRegisterClick` callbacks to `SignInRoute` and `SignInScreen`.
    - Implement a clickable annotated string in `SignInScreen` using `LinkAnnotation.Clickable` to trigger the registration flow.

--- app/src/main/java/com/ricardovalverde/droidchat/ui/feature/signIn/SignInScreen.kt
+++ app/src/main/java/com/ricardovalverde/droidchat/ui/feature/signIn/SignInScreen.kt
@@ -11,14 +11,22 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.LinkAnnotation
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextLinkStyles
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -30,13 +38,15 @@ import com.ricardovalverde.droidchat.ui.theme.DroidChatTheme
 
 @Composable
 fun SignInRoute(
-    viewModel: SignInViewModel = viewModel()
+    viewModel: SignInViewModel = viewModel(),
+    navigateToSignUp: () -> Unit
 ) {
     val formState = viewModel.formState
 
     SignInScreen(
         formState = formState,
-        onFormEvent = viewModel::onFormEvent
+        onFormEvent = viewModel::onFormEvent,
+        onRegisterClick = navigateToSignUp
     )
 }
 
@@ -44,7 +54,8 @@ fun SignInRoute(
 fun SignInScreen(
     modifier: Modifier = Modifier,
     formState: SignInFormState,
-    onFormEvent: (SignInFormEvent) -> Unit
+    onFormEvent: (SignInFormEvent) -> Unit,
+    onRegisterClick: () -> Unit
 ) {
 
 
@@ -103,6 +114,40 @@ fun SignInScreen(
                 onFormEvent(SignInFormEvent.Submit)
             },
         )
+        Spacer(modifier = Modifier.height(58.dp))
+
+        val noAccountText = stringResource(R.string.feature_login_no_account)
+        val registerText = stringResource(R.string.feature_login_register)
+        val noAccountRegisterText = "$noAccountText $registerText"
+
+        val annotatedString = buildAnnotatedString {
+            val registerTextStartIndex = noAccountRegisterText.indexOf(registerText)
+            val registerTextEndIndex = registerTextStartIndex + registerText.length
+
+            append(noAccountRegisterText)
+
+            addStyle(
+                style = SpanStyle(color = Color.White),
+                start = 0,
+                end = registerTextStartIndex
+            )
+
+            addLink(
+                clickable = LinkAnnotation.Clickable(
+                    tag = "register_text", //Apenas para identificar o link
+                    styles = TextLinkStyles(
+                        style = SpanStyle(
+                            color = MaterialTheme.colorScheme.primary,
+                            textDecoration = TextDecoration.Underline
+                        )
+                    ),
+                    linkInteractionListener = { onRegisterClick() }
+                ), start = registerTextStartIndex, end = registerTextEndIndex
+
+            )
+        }
+        Text(annotatedString)
+
     }
 }
 
@@ -112,7 +157,8 @@ fun SignInScreenPreview() {
     DroidChatTheme {
         SignInScreen(
             formState = SignInFormState(),
-            onFormEvent = {}
+            onFormEvent = {},
+            onRegisterClick = {}
         )
     }
 }
